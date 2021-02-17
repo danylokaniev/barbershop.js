083b12358bb6c63d97ff35ebf7a225a7aba45206
dkanevsky <danylokaniev@gmail.com>
2021-02-18 01:57:45 +0200

    add slider

---
+++ src/component/slider/Slider.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { images } from './images'
+import styles from './Slider.m.css'
+
+const ImageSlider = props => {
+	const [current, setCurrent] = useState(0)
+	const { length } = images
+
+	const nextSlide = () => {
+		setCurrent(current === length - 1 ? 0 : current + 1)
+	}
+
+	const prevSlide = () => {
+		setCurrent(current === 0 ? length - 1 : current - 1)
+	}
+
+	const dotClick = index => {
+		setCurrent(index)
+	}
+
+	return (
+		<section className={styles.slider}>
+			<div className={`${styles.arrow} ${styles.leftArrow}`} onClick={prevSlide} />
+			<div className={`${styles.arrow} ${styles.rightArrow}`} onClick={nextSlide} />
+			{
+				images.map((image, index) => (
+					<div
+						className={`${index === current ? `${styles.slide} ${styles.active}` : styles.slide}`}
+						key={image}
+					>
+						{
+							index === current && (
+								<img src={image} className={styles.image} />
+							)
+						}
+					</div>
+				))
+			}
+			<div className={styles.dots}>
+				{
+					images.map((image, index) => (
+						<div
+							className={`${index === current ? `${styles.dot} ${styles.activeDot}` : styles.dot}`}
+							key={image}
+							onClick={() => dotClick(index)}
+						/>
+					))
+				}
+			</div>
+		</section>
+	)
+}
+
+export default ImageSlider
